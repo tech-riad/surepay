e5f4022c15e30b2e8133cf8fa67305674219d313
Md. Riad Ahmed <110854493+tech-riad@users.noreply.github.com>
2024-11-09 21:52:23 +0600

    update

--- app/modules/admin/models/Staffs_model.php
+++ app/modules/admin/models/Staffs_model.php
@@ -12,12 +12,12 @@ class Staffs_model extends MY_Model
         $this->tb_main     = STAFFS;
     }
 
-    public function staffs($params = null, $option = null)
+    public function list_items($params = null, $option = null)
     {
         $result = null;
        
         
-        $this->db->select('id,first_name, last_name, email');
+        $this->db->select('id,first_name, last_name, email,timezone');
         $this->db->from($this->tb_main);
         $this->db->order_by('id', 'desc');
         $this->db->limit($params['limit'], 0);
@@ -26,6 +26,65 @@ class Staffs_model extends MY_Model
         
         return $result; 
     }
+
+    public function count_items($params = null, $option = null)
+    {
+        $result = null;
+        if ($option['task'] == 'count-items-group-by-status') {
+            $this->db->select('count(id) as count, status');
+            $this->db->from($this->tb_main);
+            //Search
+            if ($params['search']['field'] === 'all') {
+                $i = 1;
+                foreach ($this->field_search_accepted as $column) {
+                    if ($column != 'all') {
+                        if($i == 1){
+                            $this->db->like($column, $params['search']['query']); 
+                        }elseif ($i > 1) {
+                            $this->db->or_like($column, $params['search']['query']); 
+                        }
+                        $i++;
+                    }
+                }
+            }elseif (in_array($params['search']['field'], $this->field_search_accepted) && $params['search']['query'] != "") {
+                $this->db->like($params['search']['field'], $params['search']['query']); 
+            }
+
+            $this->db->order_by('status', 'DESC');
+            $this->db->group_by('status');
+            $query = $this->db->get();
+            $result = $query->result_array();
+        }
+
+        // Count items for pagination
+        if ($option['task'] == 'count-items-for-pagination') {
+            $this->db->select('id');
+            $this->db->from($this->tb_main);
+            if ($params['filter']['status'] != 3 && in_array($params['filter']['status'], $this->filter_accepted)) {
+                $this->db->where('status', $params['filter']['status']);
+            }
+            //Search
+            if ($params['search']['field'] === 'all') {
+                $i = 1;
+                foreach ($this->field_search_accepted as $column) {
+                    if ($column != 'all') {
+                        if($i == 1){
+                            $this->db->like($column, $params['search']['query']); 
+                        }elseif ($i > 1) {
+                            $this->db->or_like($column, $params['search']['query']); 
+                        }
+                        $i++;
+                    }
+                }
+            }elseif (in_array($params['search']['field'], $this->field_search_accepted) && $params['search']['query'] != "") {
+                $this->db->like($params['search']['field'], $params['search']['query']); 
+            }
+            $query = $this->db->get();
+            $result = $query->num_rows();
+        }
+        return $result;
+    }
+
 
 
 }
