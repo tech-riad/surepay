ec7d876549868ed89cdfdeeed8a792b373604a70
Md. Riad Ahmed <110854493+tech-riad@users.noreply.github.com>
2024-11-15 17:58:33 +0600

    update

---
+++ errors/log-2024-11-15.php
@@ -0,0 +1,15 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-11-15 12:53:38 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'sure_pay' C:\xampp\htdocs\surjopay\app\core\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2024-11-15 12:53:38 --> Unable to connect to the database
+ERROR - 2024-11-15 12:53:38 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'sure_pay' C:\xampp\htdocs\surjopay\app\core\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2024-11-15 12:53:38 --> Unable to connect to the database
+ERROR - 2024-11-15 12:53:38 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'sure_pay' C:\xampp\htdocs\surjopay\app\core\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2024-11-15 12:53:38 --> Unable to connect to the database
+ERROR - 2024-11-15 12:53:38 --> Query error: Unknown database 'sure_pay' - Invalid query: SELECT `value`
+FROM `general_options`
+WHERE `name` = 'enable_https'
+ERROR - 2024-11-15 12:53:38 --> Severity: error --> Exception: Call to a member function row() on bool C:\xampp\htdocs\surjopay\app\core\MY_Model.php 64
+ERROR - 2024-11-15 17:55:45 --> Severity: Warning --> in_array() expects parameter 2 to be array, null given C:\xampp\htdocs\surjopay\app\modules\admin\models\Staffs_model.php 49
+ERROR - 2024-11-15 17:55:45 --> Severity: Warning --> in_array() expects parameter 2 to be array, null given C:\xampp\htdocs\surjopay\app\modules\admin\models\Staffs_model.php 83
+ERROR - 2024-11-15 17:55:45 --> Severity: Warning --> in_array() expects parameter 2 to be array, null given C:\xampp\htdocs\surjopay\app\modules\admin\models\Staffs_model.php 113
